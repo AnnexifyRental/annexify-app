7c818e87e4e4c171752059a1d998a167321d8095
Anuradha Ranasinghe <anuranasinghe151@gmail.com>
2024-04-18 21:31:38 +0530

    Thumbnail image upload

--- src/components/UploadPostImage.js
+++ src/components/UploadPostImage.js
@@ -1,29 +1,55 @@
-import React from "react";
-import { View, Text, Button, Image, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Button, Image, StyleSheet, TouchableOpacity } from "react-native";
+import ImagePicker from 'react-native-image-picker';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 
 const UploadPostImage = () => {
-    return (
-        <View style={styles.container}>
-        <Image
-            source={{ uri: "https://picsum.photos/200/200" }}
-            style={styles.image}
-        />
-        <Button title="Upload Image" onPress={() => alert("Image Uploaded!")} />
-        </View>
-    );
-    }
+    const openImageLibrary = () => {
+    const options = {
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity 
+      style={styles.uploadBtnContainer}
+        onPress={openImageLibrary}
+      >
+        <Text style={styles.uploadTxt}>Upload thumbnail</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    image: {
-        width: 200,
-        height: 200,
-        marginBottom: 20,
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  uploadBtnContainer:{ 
+    height: 200,
+    width: '90%',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'lightgrey',
+    borderStyle: 'dashed',
+    borderWidth: 1,
+  },
+  uploadTxt: {
+    fontSize: 20,
+    opacity: 0.5,
+    fontWeight: 'bold',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+  },
 });
 
 export default UploadPostImage;
